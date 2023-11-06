821410dcf499bd07e3f0eb41b832bcaaa037bb7d
hari saputra <hari.saputra.dev@gmail.com>
2023-11-06 11:50:11 +0700

    update

---
+++ variabel.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string //tipe data wajib di deklarasi
+
+	name = "Hari Saputra" //tidak wajib deklarasi tipe data
+	fmt.Println(name)
+
+	name = "Gabriela"
+	fmt.Println(name)
+}
